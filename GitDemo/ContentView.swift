3404b12c3c04362651d3695760788a015cd3ee7d
Sourabh Kharte <sourabh.k@vsple.com>
2025-05-15 14:16:40 +0530

    New changes

--- GitDemo/ContentView.swift
+++ GitDemo/ContentView.swift
@@ -14,6 +14,14 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
         .padding()
     }
